012d7ebfab1ce4cebbc6402ecbaeef44829c5703
21081010039ImeldhaElzandy <125734903+21081010039ImeldhaElzandy@users.noreply.github.com>
2023-02-22 20:36:03 +0700

    Add files via upload

--- GajiPegawai.java
+++ GajiPegawai.java
@@ -1,75 +1,73 @@
 package gajipegawai;
 import java.util.Scanner;
 public class GajiPegawai {
+    public String nama, alamat,departemen, pengalaman, skill;
+    public int gaji;
+    
+    public GajiPegawai (String nama, String alamat, String departemen, String pengalaman, String skill) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.departemen = departemen;
+        this.pengalaman = pengalaman;
+        this.skill = skill;
+        this.gaji = gajiAwal();
+    }
+    
+    public int gajiAwal() {
+    int gajiAwal = 0;
+    switch(departemen) {
+        case "1":
+            gajiAwal += 8000000;
+            break;
+        case "2":
+            gajiAwal += 5000000;
+            break;
+        case "3":
+            gajiAwal += 4000000;
+            break;
+    }
+    
+    switch(pengalaman) {
+        case "1":
+            gajiAwal += 500000;
+            break;
+        case "2":
+            gajiAwal += 1000000;
+            break;
+        case "3":
+            gajiAwal += 2000000;
+    }
+    
+    switch(skill) {
+        case "1":
+            gajiAwal += 1000000;
+            break;
+        case "2":
+            gajiAwal += 2000000;
+            break;
+        case "3":
+            gajiAwal += 2500000;
+    }
+    return gajiAwal;
+}
+    
     public static void main(String[] args) {
-        String nama, alamat;
-        int departemen, pengalaman, skill, gaji;
+        String nama, alamat, departemen, pengalaman, skill;
+        int gaji;
         Scanner input = new Scanner(System.in);
         
-        System.out.print("Masukkan Nama = "); 
+        System.out.print("Masukkan Nama   = "); 
         nama = input.nextLine();
         System.out.print("Masukkan Alamat = ");
         alamat = input.nextLine();
         System.out.print("Departemen [1.IT 2.HRD 3.FINANCE] = ");
-        departemen = input.nextInt();
-        switch(departemen) {
-            case 1:
-                System.out.println("Rp 8.000.000");
-                departemen = 8000000;
-                break;
-            case 2:
-                System.out.println("Rp5.000.000");
-                departemen = 5000000;
-                break;
-            case 3:
-                System.out.println("Rp4.000.000");
-                departemen = 4000000;
-                break;
-            default:
-                System.out.println("Pilihan anda salah");
-                break;
-        } 
+        departemen = input.nextLine();
+        System.out.print("Pengalaman [1.Pemula 2.Menengah 3.Expert] = ");
+        pengalaman = input.nextLine();
+        System.out.print("Skill [1.Web 2.Mobile 3.Desktop] = ");
+        skill = input.nextLine();
         
-        System.out.print("Masukkan Pengalaman [1.Pemula; 2.Menengah; 3.Expert] = ");
-        pengalaman = input.nextInt();
-        switch(pengalaman) {
-            case 1:
-                System.out.println("Rp500.000");
-                pengalaman = 500000;
-                break;
-            case 2:
-                System.out.println("Rp1.000.000");
-                pengalaman = 1000000;
-                break;
-            case 3 :
-                System.out.println("Rp2.000.000");
-                pengalaman = 2000000;
-                break;
-            default :
-                System.out.println("Pilihan anda salah");
-                break;
-        }
-        
-        System.out.print("Masukkan Skill [1.Web; 2.Mobile; 3.Desktop] = ");
-        skill = input.nextInt();
-        switch(skill) {
-            case 1:
-                System.out.println("Rp1.000.000");
-                skill = 1000000;
-                break;
-            case 2:
-                System.out.println("Rp2.000.000");
-                skill = 2000000;
-                break;
-            case 3 :
-                System.out.println("Rp2.500.000");
-                skill = 2500000;
-                break;
-            default:
-                System.out.println("Pilihan anda salah");
-                break;
-        }
-        gaji = departemen+pengalaman+skill;
-        System.out.println("Gaji Anda = Rp "+gaji);
+        GajiPegawai penghasilan = new GajiPegawai(nama, alamat, departemen, pengalaman, skill); 
+        System.out.println("\nGaji Anda = Rp "+penghasilan.gajiAwal());
     }
 }
